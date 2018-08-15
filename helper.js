bc818dc56084ebf617baffba8dfd55c470887ce0
Rey <ray.greenm@gmail.com>
2018-08-15 13:12:52 -0500

    Rey

---
+++ helper.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("first").innerHTML);
+console.log(document.querySelectorAll(".special"));
+
+document.querySelector("h1").style.color = "red";
+document.querySelector("p").style.color = "yellow";
