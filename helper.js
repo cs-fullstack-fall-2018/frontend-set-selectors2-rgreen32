a9221fd0f430303302a7ebd8b995b8e07dcfd43c
Kenn <kenn+git@code-crew.org>
2018-08-15 16:37:17 -0500

    Graded 08-15-2018 16:35:46

--- helper.js
+++ helper.js
@@ -3,3 +3,16 @@ console.log(document.querySelectorAll(".special"));
 
 document.querySelector("h1").style.color = "red";
 document.querySelector("p").style.color = "yellow";
+
+/*
+Question 1:
+	a) The p tag is not called by Tag or Class
+Questoin 2: 
+	a) The class special is not called by querySelector. Only called by querySelectorAll
+Question 3:
+	a) 	The h1 color is wrong
+Question 4:
+	a) The past p tag is not being changed to yellow.
+
+From Kenn
+*/
